870d2cc84230b7441ba598090b995b1803b1302e
alvexx45 <bernardoalves222@gmail.com>
2025-07-14 16:56:27 -0300

    update index

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -2,6 +2,7 @@ import { Text, View, Image, TouchableOpacity } from "react-native"
 import { MaterialIcons } from "@expo/vector-icons"
 import { styles } from "./styles"
 import { colors } from "@/styles/colors"
+import { Category } from "@/components/category"
 
 export default function Index(){
     return (
@@ -13,7 +14,9 @@ export default function Index(){
                     <MaterialIcons name="add" size={32} color={colors.green[300]} />
                 </TouchableOpacity>
             </View>
+            <Category/>
         </View>
+
     )
 }
 
