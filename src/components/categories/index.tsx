d3d683df6f3d141ac3fa72e5d73da1b93e579759
alvexx45 <bernardoalves222@gmail.com>
2025-07-16 14:55:33 -0300

    flat list

---
+++ src/components/categories/index.tsx
@@ -0,0 +1,16 @@
+import { FlatList } from "react-native";
+import { styles } from "./styles";
+import { categories } from "@/utils/categories";
+import { Category } from "../category";
+
+export function Categories() {
+    return (
+        <FlatList
+            data={categories}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+                <Category name={item.name} icon={item.icon} isSelected={false} />
+            )}
+        />
+    )
+}
