c6a8999d62206124c5d2a432035ca32a7e243b67
alvexx45 <bernardoalves222@gmail.com>
2025-07-16 14:37:59 -0300

    isSelected

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -9,15 +9,13 @@ export default function Index(){
         <View style={styles.container}>
             <View style={styles.header}>
                 <Image source={require("@/assets/logo.png")} style={styles.logo} />
-
                 <TouchableOpacity>
                     <MaterialIcons name="add" size={32} color={colors.green[300]} />
                 </TouchableOpacity>
             </View>
-            <Category name="Projects" icon="code"/>
-            <Category name="Site" icon="language"/>
-            <Category name="Video" icon="movie"/>
+            <Category name="Projects" icon="code" isSelected={true} />
+            <Category name="Site" icon="language" isSelected={false} />
+            <Category name="Video" icon="movie" isSelected={false} />
         </View>
     )
 }
-
