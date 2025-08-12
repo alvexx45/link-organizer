33990ba79541bd10dc8281003e800cdca46a5b1b
alvexx45 <bernardoalves222@gmail.com>
2025-08-12 16:39:34 -0300

    usefocuseffect

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -23,9 +23,9 @@ export default function Index() {
         }
     }
 
-    useEffect(() => {
+    useFocusEffect(useCallback(() => {
         getLinks()
-    }, [category])
+    }, []))
 
     return (
         <View style={styles.container}>
